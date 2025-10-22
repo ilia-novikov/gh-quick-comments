dfefe997d2c280e455c9e36fdce0a12bcab8379b
Ilia Novikov <3877055+ilia-novikov@users.noreply.github.com>
2025-10-22 15:44:47 +0200

    A draft of adding a button

--- quick_comments.js
+++ quick_comments.js
@@ -3,7 +3,28 @@ var wasCommentSectionUpdated = false;
 function onCommentSectionAdded(el) {
   wasCommentSectionUpdated = true;
 
-  console.log(el.children[0].children[0]);
+  const container = el.children[0].children[0];
+  const link = document.createElement("a");
+
+  link.href = "javascript:void(0);";
+  link.setAttribute("data-name", "Kaizen");
+  link.setAttribute("data-view-component", "true");
+  link.className = "IssueLabel hx_IssueLabel width-fit mb-1 mr-1";
+
+  link.style.setProperty("--label-r", "121");
+  link.style.setProperty("--label-g", "230");
+  link.style.setProperty("--label-b", "184");
+  link.style.setProperty("--label-h", "154");
+  link.style.setProperty("--label-s", "68");
+  link.style.setProperty("--label-l", "68");
+
+  const span = document.createElement("span");
+  span.className = "css-truncate css-truncate-target width-fit";
+  span.setAttribute("label", "Blocking");
+  span.textContent = "❗";
+
+  link.appendChild(span);
+  container.appendChild(link);
 }
 
 window.addEventListener("pageshow", function () {
