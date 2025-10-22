e88f58fd8bb98f82b6cfa497e5809f169d456aa7
Ilia Novikov <3877055+ilia-novikov@users.noreply.github.com>
2025-10-22 15:53:52 +0200

    More generic link rendering

--- quick_comments.js
+++ quick_comments.js
@@ -1,30 +1,41 @@
+const COMMENTS = [
+  {
+    icon: "❗",
+    title: "Blocking: change requested or at least discussion required",
+    rgb: [121, 230, 184],
+    hsl: [154, 68, 68],
+  },
+];
+
 var wasCommentSectionUpdated = false;
 
 function onCommentSectionAdded(el) {
   wasCommentSectionUpdated = true;
 
   const container = el.children[0].children[0];
-  const link = document.createElement("a");
+  for (var i = 0; i < COMMENTS.length; i++) {
+    comment = COMMENTS[i];
+    const link = document.createElement("a");
 
-  link.href = "javascript:void(0);";
-  link.setAttribute("data-name", "Kaizen");
-  link.setAttribute("data-view-component", "true");
-  link.className = "IssueLabel hx_IssueLabel width-fit mb-1 mr-1";
+    link.title = comment.title;
+    link.href = "javascript:void(0);";
+    link.setAttribute("data-view-component", "true");
+    link.className = "IssueLabel hx_IssueLabel width-fit mb-1 mr-1";
 
-  link.style.setProperty("--label-r", "121");
-  link.style.setProperty("--label-g", "230");
-  link.style.setProperty("--label-b", "184");
-  link.style.setProperty("--label-h", "154");
-  link.style.setProperty("--label-s", "68");
-  link.style.setProperty("--label-l", "68");
+    link.style.setProperty("--label-r", comment.rgb[0].toString());
+    link.style.setProperty("--label-g", comment.rgb[1].toString());
+    link.style.setProperty("--label-b", comment.rgb[2].toString());
+    link.style.setProperty("--label-h", comment.hsl[0].toString());
+    link.style.setProperty("--label-s", comment.hsl[1].toString());
+    link.style.setProperty("--label-l", comment.hsl[2].toString());
 
-  const span = document.createElement("span");
-  span.className = "css-truncate css-truncate-target width-fit";
-  span.setAttribute("label", "Blocking");
-  span.textContent = "❗";
+    const span = document.createElement("span");
+    span.className = "css-truncate css-truncate-target width-fit";
+    span.textContent = comment.icon;
 
-  link.appendChild(span);
-  container.appendChild(link);
+    link.appendChild(span);
+    container.appendChild(link);
+  }
 }
 
 window.addEventListener("pageshow", function () {
