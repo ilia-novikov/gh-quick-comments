f4d7e947cd4a11c48c79557def323dd33a62d234
Ilia Novikov <3877055+ilia-novikov@users.noreply.github.com>
2025-10-22 15:30:06 +0200

    MutationObserver

--- quick_comments.js
+++ quick_comments.js
@@ -1,4 +1,25 @@
-window.addEventListener("load", function () {
-  console.log("loaded!");
-});
+function onCommentSectionAdded(el) {
+  console.log(el);
+}
 
+window.addEventListener("load", function () {
+  console.log("GH Quick Comments: loaded");
+
+  var observer = new MutationObserver(function (mutations) {
+    const commentSection = document.querySelector(
+      'div[data-marker-id="new-comment"]'
+    );
+    if (!commentSection) {
+      return;
+    }
+
+    onCommentSectionAdded(commentSection);
+  });
+
+  observer.observe(document, {
+    attributes: false,
+    childList: true,
+    characterData: false,
+    subtree: true,
+  });
+});
